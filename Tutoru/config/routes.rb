45118019c474274c1c60092dd1e8b8a57adcf025
AsakuraRET <emelyatop@gmail.com>
2019-05-29 14:55:32 +0300

    different type

---
+++ Tutoru/config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  devise_for :users, controllers: {
+   registrations: 'users/registrations'
+  }
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: "application#landing"
+  
+  get '/home' => 'application#home'
+end
